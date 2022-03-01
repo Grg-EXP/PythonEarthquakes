4b9c6aba56d1c083821f59bdd9ead7c89dd481f1
Grg-EXP <67336954+Grg-EXP@users.noreply.github.com>
2022-03-02 00:04:22 +0100

    added audio functions

--- _earthquake.py
+++ _earthquake.py
@@ -1,53 +1,84 @@
-from urllib.request  import urlopen
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from playsound import playsound
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import math
 import time
+
 
 def getValueFromWebSite():
     response = urlopen('http://terremoti.ingv.it/')
     #response = urlopen('https://www.3bmeteo.com/terremoti/italia')
-    soup = BeautifulSoup(response.read(),'html.parser')
+    soup = BeautifulSoup(response.read(), 'html.parser')
 
-    #print(soup.body)
-    #print(soup.td)
-    #print(soup.find_all('td'))
-
+    # print(soup.body)
+    # print(soup.td)
+    # print(soup.find_all('td'))
 
     for line in soup.find('td', {'class': 'text-center'}).stripped_strings:
         if 'Mwp' in line:
             value = float(line.partition('Mwp')[2])
-            if (value!=None):
+            if (value != None):
                 return(value)
         if 'ML' in line:
             value = float(line.partition('ML')[2])
-            if (value!=None):
+            if (value != None):
                 return(value)
 
 
 def getValueOnlyFromItalyFromWebSite():
     response = urlopen('http://terremoti.ingv.it/')
     #response = urlopen('https://www.3bmeteo.com/terremoti/italia')
-    soup = BeautifulSoup(response.read(),'html.parser')
+    soup = BeautifulSoup(response.read(), 'html.parser')
 
-    #print(soup.body)
-    #print(soup.td)
-    #print(soup.find_all('td'))
+    # print(soup.body)
+    # print(soup.td)
+    # print(soup.find_all('td'))
 
     lines = soup.find_all('td', {'class': 'text-center'})
 
     for line in lines:
         if 'ML' in line.get_text():
             value = float(line.get_text().partition('ML')[2])
-            if (value!=None):
+            if (value != None):
                 return(value)
 
 
+def setAudio(intensity):
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(
+        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    # Get current volume
+    currentVolumeDb = volume.GetMasterVolumeLevel()
+    volume.SetMasterVolumeLevel(currentVolumeDb - intensity, None)
+
+
+def playAudio(path, intensity):
+    setAudio(intensity)
+    playsound(path)
+
+
 for i in range(100):
+    magnitude = getValueOnlyFromItalyFromWebSite()
     print('world: ' + str(getValueFromWebSite()))
     print('italy: ' + str(getValueOnlyFromItalyFromWebSite()))
-    time.sleep(20)
+
+    setAudio(5)
+    time.sleep(1)
 
 
+'''
 
+3.0–3.9	minor	felt by many people; no damage	12,000–100,000
+4.0–4.9	light	felt by all; minor breakage of objects	2,000–12,000
+5.0–5.9	moderate	some damage to weak structures	200–2,000
+6.0–6.9	strong	moderate damage in populated areas	20–200
+7.0–7.9	major	serious damage over large areas; loss of life	3–20
+8.0 and higher	great	severe destruction and loss of life over large areas	fewer than 3
+'''
 
 '''
 <div class="panel-body">      
